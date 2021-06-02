fc63394b2be8d5ec9b8958851dde10f0c3e6a055
pe4enko01 <pe4enko02@yandex.ru>
2021-06-02 13:16:40 +0300

    f

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter} from "react-router-dom"
 import App from './App';
+
 
 test('renders learn react link', () => {
   render(<App />);
